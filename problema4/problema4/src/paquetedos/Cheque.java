bf07109a9e9a411d082c2537d8aa32228206053b
Alex Quishpe <alexquishpe45@gmail.com>
2023-05-04 13:00:37 -0500

    subida2

--- problema4/problema4/src/paquetedos/Cheque.java
+++ problema4/problema4/src/paquetedos/Cheque.java
@@ -1,11 +1,65 @@
-
 package paquetedos;
 
-
 public class Cheque {
-      private String ced;
-    private String marcaVehiculo;
-    private int anioFabricacion;
-    private double valVehiculo;
-    
+
+    private String nombreC;
+    private String nombreB;
+    private double valorCh;
+    private double comision;
+
+    public Cheque(String nC) {
+        nombreC = nC;
+    }
+
+    public Cheque(String nC, String nB, double vCh) {
+        nombreC = nC;
+        nombreB = nB;
+        valorCh = vCh;
+    }
+    // 
+
+    public void establecerNombreC(String nC) {
+        nombreC = nC;
+    }
+
+    public void establecerNombreB(String nB) {
+        nombreB = nB;
+    }
+
+    public void establecerValorCh(double vCh) {
+        valorCh = vCh;
+    }
+
+    public void calcularComision() {
+        comision = valorCh * 0.00003;
+    }
+
+    // Obtener
+    public String obtenerNombreC() {
+        return nombreC;
+    }
+
+    public String obtenertNombreB() {
+        return nombreB;
+    }
+
+    public double obtenerValorCh() {
+        return valorCh;
+    }
+
+    public double obtenerComision() {
+        return comision;
+    }
+
+    @Override
+    public String toString() {
+
+        String cadena = String.format("Nombre: %s\nNombre del Banco: %s\nValor del cheque: %.2f\n"
+                + "Comision: %.2f\n", nombreC,
+                nombreB,
+                valorCh,
+                comision);
+
+        return cadena;
+    }
 }
--- problema4/problema4/src/paqueteuno/Ejecutor.java
+++ problema4/problema4/src/paqueteuno/Ejecutor.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package paqueteuno;
 
-/**
- *
- * @author alexj
- */
+import paquetedos.Cheque;
 public class Ejecutor {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+              Cheque ch= new Cheque("Ronaldo Sanchez");
+       Cheque ch2 = new Cheque("Jean Maldonado","Banco de Loja",2789);
+       ch.establecerNombreC("113876234");
+       ch.establecerNombreB("Chévrolet");
+       ch.establecerValorCh(2009);
+       ch.calcularComision();
+       ch2.calcularComision();
+       System.out.println("Imprimir Consturctor Uno");
+       System.out.println(ch);
+       System.out.println("------------------------------------------");
+       System.out.println("Imprimir Consturctor  Dos");
+      System.out.println(ch2);
     }
     
 }
